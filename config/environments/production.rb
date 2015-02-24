d3042d697200fb8fa9f5911df6536a80cafb7479
raysango <remongeorge@live.com>
2015-02-24 08:24:02 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = {host: 'https://rayblocmetrics.herokuapp.com/'}
+  
+  config.action_mailer.default_url_options = { host: 'https://rayblocmetrics.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
