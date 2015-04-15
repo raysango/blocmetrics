d9b22efc7507025dea37d70cc7eaeb16f6a4ae2b
Raymond Morcos <remongeorge@live.com>
2015-04-14 22:46:01 -0700

    background_destroy_update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = {host: 'http://blue-motor-90-164714.usw1-2.nitrousbox.com/'}
+  config.action_mailer.default_url_options = {host: 'localhost:3000'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
